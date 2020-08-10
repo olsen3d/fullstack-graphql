88ec006156f742a0a051cc04a4d7f612e795d853
Michael Olsen <molsen@Michaels-Air.lan>
2020-08-09 21:50:34 -0400

    new pet

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 import React, {useState} from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
@@ -5,15 +6,43 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
+const CREATE_PET = gql`
+mutation CreateAPet($newPet: NewPetInput!) {
+  addPet(input: $newPet) {
+    id
+    name
+    type
+    img
+  }
+}
+`
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const {data, loading, error} = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(CREATE_PET)
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {newPet: input}
+    })
   }
-  
+
+  if (loading || newPet.loading) return <Loader />
+  if (error || newPet.error) return <h2>ERROR</h2>
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +61,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
